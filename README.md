6ded1daf0ea57f0369aeb545f8bb1d9e1c723f88
Tien Vu <tienvu461@gmail.com>
2021-01-13 22:22:55 +0700

    create docker compose

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # myFirstBlog
 trying to host my first blog
+
+
+CREATE DATABASE blogdb;
+CREATE USER blogdb WITH ENCRYPTED PASSWORD 'blogdb123';
+ALTER ROLE blogdb SET client_encoding TO 'utf8';
+ALTER ROLE blogdb SET default_transaction_isolation TO 'read committed';
+ALTER ROLE blogdb SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE blogdb TO blogdb;
--- myFirstBlog/myFirstBlog/settings.py
+++ myFirstBlog/myFirstBlog/settings.py
@@ -73,15 +73,16 @@ WSGI_APPLICATION = 'myFirstBlog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
